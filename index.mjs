0bb64783e16dcb3debf7946c20ad80635a758f99
Vishal Singh <vishal@aerinit.com>
2025-06-23 15:01:14 +0530

    updated code

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from daddy!'),
+    body: JSON.stringify('Hello from Vishal!'),
   };
   return response;
 };
