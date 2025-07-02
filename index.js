f2cafe996954a358eff58f98c92223a6683ae4be
VishalAerinIt <vishal@aerinit.com>
2025-07-02 15:01:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello Manish Sir'),
+    body: JSON.stringify('Hello DevOps'),
   };
   return response;
 };
